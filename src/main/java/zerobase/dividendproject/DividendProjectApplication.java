e2f9cf6d910c8389216ef9238e4108cda35761bb
Hyunju Lee <guswnee00@gmail.com>
2024-03-27 17:07:38 +0900

    scheduling fin

--- src/main/java/zerobase/dividendproject/DividendProjectApplication.java
+++ src/main/java/zerobase/dividendproject/DividendProjectApplication.java
@@ -2,8 +2,10 @@ package zerobase.dividendproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DividendProjectApplication {
 
     public static void main(String[] args) {
